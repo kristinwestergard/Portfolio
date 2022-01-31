3839b33ed751ddb14fa479bee5fe5576c3423463
kristinwestergard <kristin.westergard@chasacademy.se>
2022-01-31 15:34:17 +0100

    javascript buttons, added content projekt 1

---
+++ script.js
@@ -0,0 +1,14 @@
+function overLinkedIn(mOver){
+    mOver.innerHTML = "LinkedIn öppnas i ny flik";
+}
+function outLinkedIn(mOver){
+    mOver.innerHTML = "Kontakta mig på LinkedIn";
+}
+function overMail(mOver){
+    mOver.innerHTML = "Mail-applikation öppnas";
+}
+
+function outMail(mOver){
+    mOver.innerHTML = "Maila mig";
+}
+
